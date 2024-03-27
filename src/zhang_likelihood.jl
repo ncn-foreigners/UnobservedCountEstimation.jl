a436b00d86aa2262402864cb7c720e3f4b1a56e6
Piotr Chlebicki <piochl@st.amu.edu.pl>
2024-03-27 12:26:30 +0100

    log link on ϕ

--- src/zhang_likelihood.jl
+++ src/zhang_likelihood.jl
@@ -1,11 +1,13 @@
 # likelihood and derivatives go here
 
 function log_lik_original_model(α, β, ϕ, m, N, n, X, Z)
+    ϕ = exp(ϕ)
     μ = (N .^ (X * α)) .* ((n ./ N) .^ (Z * β))
     sum(m .* log.(μ) .- (m .+ ϕ) .* log.(μ .+ ϕ) .+ ϕ .* log.(ϕ) .- loggamma.(ϕ) .- loggamma.(m .+ 1) .+ loggamma.(m .+ ϕ))
 end # end function
 
 function grad_log_lik_original_model(α, β, ϕ, m, N, n, X, Z)
+    ϕ = exp(ϕ)
     # SpecialFunctions.jl has digamma and trigamma function
     μ = (N .^ (X * α)) .* ((n ./ N) .^ (Z * β))
     dμ = m ./ μ .- (ϕ .+ m) ./ (μ .+ ϕ)
@@ -13,18 +15,25 @@ function grad_log_lik_original_model(α, β, ϕ, m, N, n, X, Z)
     dα = dμ .* μ .* log.(N)
     dβ = dμ .* μ .* log.(n ./ N)
     dϕ = sum(SpecialFunctions.digamma.(ϕ .+ m) .- log.(μ .+ ϕ) .+ log.(ϕ) .- SpecialFunctions.digamma.(ϕ) .+ 1 .- (ϕ .+ m) ./ (ϕ .+ μ))
+    dϕ *= ϕ # log link
 
     vcat(X' * dα, Z' * dβ, dϕ)
 end # end function
 
 function hess_log_lik_original_model(α, β, ϕ, m, N, n, X, Z)
+    ϕ = exp(ϕ)
     μ = (N .^ (X * α)) .* ((n ./ N) .^ (Z * β))
     dμ = m ./ μ .- (ϕ .+ m) ./ (μ .+ ϕ)
     dμ_2 = (ϕ .+ m) ./ (μ .+ ϕ) .^ 2 .- m ./ μ .^ 2
     dμdϕ = - (μ .- m) ./ (μ .+ ϕ) .^ 2
+    # log link
+    dμdϕ *= ϕ
 
     #dϕ = sum(SpecialFunctions.digamma.(ϕ .+ m) .- log.(μ .+ ϕ) .+ log.(ϕ) .- SpecialFunctions.digamma.(ϕ) .+ 1 .- (ϕ .+ m) ./ (ϕ .+ μ))
-    dϕ_2 = sum(SpecialFunctions.trigamma.(ϕ .+ m) .- SpecialFunctions.trigamma.(ϕ) .- 2 ./ (ϕ .+ μ) .+ (ϕ .+ m) ./ (ϕ .+ μ) .^ 2 .+ ϕ ^ -1)
+    dϕ_2  = sum(SpecialFunctions.trigamma.(ϕ .+ m) .- SpecialFunctions.trigamma.(ϕ) .- 2 ./ (ϕ .+ μ) .+ (ϕ .+ m) ./ (ϕ .+ μ) .^ 2 .+ ϕ ^ -1)
+    # log link
+    dϕ_2 *= ϕ ^ 2
+    dϕ_2 += sum(SpecialFunctions.digamma.(ϕ .+ m) .- log.(μ .+ ϕ) .+ log.(ϕ) .- SpecialFunctions.digamma.(ϕ) .+ 1 .- (ϕ .+ m) ./ (ϕ .+ μ)) * ϕ
 
     dα_2 = dμ_2 .* (μ .* log.(N)) .^ 2 .+ dμ .* μ .* (log.(N) .^ 2)
     dβ_2 = dμ_2 .* (μ .* log.(n ./ N)) .^ 2 .+ dμ .* μ .* (log.(n ./ N) .^ 2)
